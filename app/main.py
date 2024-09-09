0d77771c9792cb77c8ebfcbf07a0a94e4266bdb7
Rohitrky2021 <102401490+Rohitrky2021@users.noreply.github.com>
2024-09-10 01:31:08 +0530

    ADDED RAG

--- app/main.py
+++ app/main.py
@@ -2,7 +2,19 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.models import PipelineData  # Absolute import
 from app.crud import parse_pipeline  # Absolute import
-
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+import google.generativeai as genai
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
 app = FastAPI()
 
 # Add CORS Middleware
@@ -29,6 +41,63 @@ def read_root():
 @app.post('/pipelines/parse')
 def handle_pipeline(pipeline: PipelineData):
     return parse_pipeline(pipeline)
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    return response
+
+@app.post('/add_data')
+def chat_pdf(data:str):
+    chunks = get_text_chunks(data)
+    get_vector_store(chunks)
+    return {"message": "Data added successfully"}
+
+@app.post('/chat')
+def chat_pdf2(ques:str):
+    chain = get_conversational_chain()
+    response = user_input(ques)
+    return {'message': response}
 
 
 
